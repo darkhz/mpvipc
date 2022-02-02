58758ad355dd414625ebb357ba64484340ee04ca
darkhz <kmachanwenw@gmail.com>
2022-02-02 21:04:21 +0530

    Add comment

--- pipe_windows.go
+++ pipe_windows.go
@@ -9,5 +9,7 @@ import (
 )
 
 func dial(path string) (net.Conn, error) {
+	// npipe's Dial normally blocks indefinitely until a
+	// connection is found. Use DialTimeout.
 	return npipe.DialTimeout(path, 1000)
 }
